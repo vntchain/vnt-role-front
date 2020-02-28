ecfc345f0321a667b48cb3f750b99a0706b21980
zhouxs <zhouxs@yunphant.com>
2020-02-28 18:31:11 +0800

    fix: 调整indexcss加载顺序

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import './index.css';
 import App from './containers/App.jsx';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 import { Provider } from './components/Context'
 
